e34ceed509a20d9bfce20adebfa4df55f019cb0d
Annely Kurg <annely.kurg@khk.ee>
2022-04-06 11:15:59 +0300

    templating directory setup

--- index.js
+++ index.js
@@ -1,29 +1,14 @@
 let express = require('express');
+//require path
+const path = require('path')
 const app = express();
 //add public directory
 app.use(express.static('public'));
-/*//set up brouther address row router
-app.get('/', (req, res)=> {
-//create html response
-    res.send('<a href="/contact">Contact Us</a> <br> <a href="/about">About Us</a>');
-});
+//add views directory path
+app.set('views', path.join(__dirname, 'views'));
+//add view template engine
+app.set('view engine', 'ejs')
 
-app.get('/contact', (req, res)=> {
-    res.send('<h1>Contact Us Page</h1>');
-});
-
-app.get('/about', (req, res)=> {
-    res.send('<h1>About Us Page</h1>');
-});
-
-app.get('*', (req, res)=> {
-    res.send('404. This page does not exist. <a href="/">Go To HomePage</a>');
-});*/
-
-/*app.get('/demo', (req, res)=> {
-//create html response
-    res.render('index.ejs');
-});*/
 
 app.get('/user/:username', (req, res) => {
     //get parameter data from addressrow
